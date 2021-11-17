d1ebe5b777377bbc554ea0ed520fc02e5aa330a1
swaritkhare <swaritkhare@gmail.com>
2021-11-17 16:12:51 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState("");
+  const clientId = "x82vIePhkV7BJIYN10AYmmNUamjxbZSubRtdVxcw8Is";
+  const [result, setResult] = useState([]);
+  const handleChange = (event) => {
+    setImage(event.target.value);
+  };
+  const handleSubmit = () => {
+    console.log(image);
+    const url =
+      "https://api.unsplash.com/search/photos?page=1&query=" +
+      image +
+      "&client_id=" +
+      clientId;
+    axios.get(url).then((response) => {
+      console.log(response);
+      setResult(response.data.results);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="input">
+          <input
+            onChange={handleChange}
+            type="text"
+            name="image"
+            className="input1"
+            placeholder="Search for images"
+          />
+          <button onClick={handleSubmit} type="submit" className="btn">
+            Search
+          </button>
+        </div>
+
+        <div className="result">
+          {result.map((image) => (
+            <>
+              <div className="card" key={image.id}>
+                <img
+                  alt={image.alt_description}
+                  src={image.urls.full}
+                  className="card--image"
+                  width="50%"
+                  height="50%"
+                />
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
